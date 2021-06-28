c8765bb706d990e73229de2e60c367c3c90471a7
Roman Brazhnyk <rbrazhnyk@gmail.com>
2021-06-28 16:39:26 +0800

    refactor: update API endpoints

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 const appUUID = 'test'
 const issuerStatus = 'VERIFIED'
 
-const apiLookUpHost = 'https://01djq2yvy8.execute-api.us-west-1.amazonaws.com'
-const issuerLookUpPath = '/prod/issuers/lookup'
-const vaccineNameLookUpPath = '/prod/vaccines'
+const apiLookUpHost = 'https://shc.tcp.affinidi.com'
+const issuerLookUpPath = '/api/v1/issuers/lookup'
+const vaccineNameLookUpPath = '/api/v1/vaccines'
 
 export const issuerNameLookUpUrl = `${apiLookUpHost}${issuerLookUpPath}?appUUID=${appUUID}&status=${issuerStatus}`
 export const vaccineNameLookUpUrl = `${apiLookUpHost}${vaccineNameLookUpPath}`
