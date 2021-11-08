b7926569b1dc4cd7c5fb82a6cd2514fb4026fbd2
Carlos Ponce <carlos@thecommonsproject.org>
2021-11-08 04:20:31 -0800

    SHCV-5 - Fixing style for ADA.

--- src/components/customButton.tsx
+++ src/components/customButton.tsx
@@ -41,6 +41,7 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     alignSelf: 'stretch',
     marginRight: 10,
+    textAlign: "center"
   },
   appButtonImage: {
     maxHeight: 26 * PixelRatio.getFontScale(),
