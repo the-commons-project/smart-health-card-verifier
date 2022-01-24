b6eb6088bfded090dbb09a4e045546c38cba145b
Yuri Fukuda <yuri@thecommonsproject.org>
2022-01-24 09:42:25 -0500

    localize module test mock support

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -12,7 +12,6 @@ import renderer from 'react-test-renderer';
 jest.mock("react-native-localize", () => {
   return {
     getLocales: jest.fn(),
-    // you can add other functions mock here that you are using
   };
 });
 
