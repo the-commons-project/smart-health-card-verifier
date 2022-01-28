64b537d633571e711b8d116ab266f0de23273df3
cp-yfukuda <90639249+cp-yfukuda@users.noreply.github.com>
2022-01-28 09:48:43 -0500

    translationfix (#55)
    * translationfix

--- src/container/VerificationResultPage.tsx
+++ src/container/VerificationResultPage.tsx
@@ -59,7 +59,7 @@ const VerificationResultPage = ({ route, navigation }: Props) => {
         {showResult && validationResult.isValid && <ResultRecord data={data} />}
       </ScrollView>
       <AppButton
-        title={t("Result.ScanNext","Scan next vaccination record")}
+        title={t("Common.ScanNext","Scan next vaccination record")}
         onPress={() => navigation.navigate('ScanQR')}
         backgroundColor="#255DCB"
       />
