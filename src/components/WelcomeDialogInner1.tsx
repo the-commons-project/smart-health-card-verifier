f2b5a33b07c8755c6492382f4d5ecc062b0b4e25
Eddie Cruz <38390406+edwardjcruz@users.noreply.github.com>
2022-04-11 12:27:52 -0400

    completed onboarding components

--- src/components/WelcomeDialogInner1.tsx
+++ src/components/WelcomeDialogInner1.tsx
@@ -1,34 +1,90 @@
 import React from 'react';
 import { View, StyleSheet, Text, Image, useWindowDimensions, PixelRatio } from 'react-native';
-import AppLogoSVG from '../../assets/img/icon_shc.svg'
-const width =300
 import FontStyle from '../utils/FontStyleHelper'
 import { useTranslation } from '../services/i18n/i18nUtils' 
 
+
+const images = {
+    warning: require('../../assets/img/verificationresult/warning.png'),
+    success: require('../../assets/img/verificationresult/success.png'),
+    fail: require('../../assets/img/verificationresult/fail.png'),
+    tick: require('../../assets/img/verificationresult/tick.png'),
+    cross: require('../../assets/img/verificationresult/cross.png'),
+  }
+const width = 300 
+
 export default WelcomeDialogInner1 = ({ width }:{width: number})=> {
-    //const { width } = useWindowDimensions();
     const { t } = useTranslation()
+    let verifiedColor = '#0E6B23'
+    let warningColor = '#EA6300'
+    let notVerifiedColor = '#C33E38'
+    
     return (
         <View style={[styles.container, { width }]}>
         <View>
             <Text style={styles.title}> { t('WelcomeDialog.HowItWorks', 'How it works') }</Text>
             <View>
-            <Text style={styles.description}>Scan a SMART Health Card and one of three results will appears:</Text>
-            <Image style={ styles.verificationResults } source={require('../../assets/img/issuerVerified.png')}/>
+            <Text style={styles.description}>{ t('WelcomeDialog.WelcomeInner1Description1', "Scan a SMART Health Card and one of three results will appear:" ) }</Text>
+            <View style={[ styles.bannerContainer, styles.bannerContainerClosed, { backgroundColor: verifiedColor } ] }>
+            <View style={ [styles.flexColumnContainer, ,{ width:width }] } >
+            <Image style={ styles.bannerImage } source={ images.success } />
+                <Text
+
+                  style={ [
+                    styles.bannerText,
+                    FontStyle.Poppins_600SemiBold,
+                    { backgroundColor: verifiedColor },
+                  ] }
+                >
+                    
+                { t('Result.Verified', 'Verified') }
+                </Text>
+                </View>
             </View>
             <View>
-            <Text style={styles.description}>The SMART Health Card is valid and issued by a provider in the CommonTrust Network.</Text>
-            <Image source={require('../../assets/img/issuerNotRecognized.png')}/>
             </View>
-            <Text style={styles.description}>The SMART Health Card is valid but is not issued by a provider in the CommonTrust Network. Proceed with caution.</Text>
+            <Text style={styles.description}>{ t('WelcomeDialog.VerifiedWelcomeDialog', "The SMART Health Card is valid and issued by a provider in the CommonTrust Network.") }</Text>
+            <View style={[ styles.bannerContainer, styles.bannerContainerClosed, { backgroundColor: warningColor } ] }>
+            <View style={ [styles.flexColumnContainer, ,{ width:width }] } >
+            <Image style={ styles.bannerImage } source={ images.warning } />
+                <Text
+
+                  style={ [
+                    styles.bannerText,
+                    FontStyle.Poppins_600SemiBold,
+                    { backgroundColor: warningColor },
+                  ] }
+                >
+                    
+                   { t('Result.IssuerNotRecognized', 'Issuer Not Recognized') }
+                </Text>
+                </View>
+            </View>
+            </View>
+            <Text style={styles.description}> { t('WelcomeDialog.PartiallyVerifiedWelcomeDialog', "The SMART Health Card is valid but is not issued by a provider in the CommonTrust Network. Proceed with caution.")} </Text>
             <View>
-            <Image source={require('../../assets/img/shcNotVerified.png')}/>
-            <Text style={styles.description}>The SMART Health Card could be verified. It may have been corrupted or tampered with.</Text>
+            <View style={[ styles.bannerContainer, styles.bannerContainerClosed, { backgroundColor: notVerifiedColor } ] }>
+            <View style={ [styles.flexColumnContainer, ,{ width:width }] } >
+            <Image style={ styles.bannerImage } source={ images.fail } />
+                <Text
+
+                  style={ [
+                    styles.bannerText,
+                    FontStyle.Poppins_600SemiBold,
+                    { backgroundColor: notVerifiedColor },
+                  ] }
+                >
+                    
+                   { t('Result.NotVerified', 'Not Verified') }
+                </Text>
+                </View>
+            </View>
+            <Text style={styles.description}> { t('WelcomeDialog.NotVerifiedWelcomeDialog', "The SMART Health Card could not be verified. It may have been corrupted or tampered with.") }</Text>
             
             </View>
         </View>
     </View>
-    
+
         
     );
 };
@@ -39,28 +95,24 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    image: {
-        flex: 0.7,
-        justifyContent: 'center'
-    },
     title: {
-        fontWeight: '800',
+        fontWeight: '700',
         fontSize: 28,
         marginBottom: 10,
-        color: '#493d8a',
+        color: '#052049',
         textAlign: 'center'
     },
     description: {
         fontWeight: '300',
         color: '#62656b',
         textAlign: 'center',
-        paddingHorizontal: 0
+        paddingHorizontal: 12,
+        paddingTop: 10
     },
     innerComponent: {
         backgroundColor: `#7fffd4`,
         flex: 1,
         alignItems: 'center',
-        width: '100%'
     },
     textContainer: {
         marginTop: 20,
@@ -70,11 +122,64 @@ const styles = StyleSheet.create({
         width: 150, height: 150, resizeMode: 'contain'
     },
     verificationResults: {
-        
         alignItems: 'center',
         resizeMode: 'contain',
         justifyContent: 'center',
-        
-        
-    }
+    },
+    bannerContainer: {
+        backgroundColor: '#67AC5B',
+        borderTopLeftRadius: 4,
+        borderTopRightRadius: 4,
+        width: 300,
+        minHeight: 56,
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginTop: 16,
+      },
+      bannerContainerClosed: {
+        borderBottomLeftRadius: 4,
+        borderBottomRightRadius: 4,
+        width: 300,
+        marginLeft: 10,
+        marginRight: 10
+      },
+      bannerImage: {
+        marginLeft: 16,
+        marginRight: 16,
+        height: 40,
+        width: 40,
+      },
+      bannerText: {
+        fontSize: 20,
+        color: '#FFFFFF',
+        lineHeight: 33,
+        alignSelf: 'center',
+      },
+      subBannerContainer: {
+        backgroundColor: '#FFFFFF',
+        borderColor: '#000',
+        borderStyle: 'solid',
+        borderWidth: 1.5,
+        borderBottomLeftRadius: 4,
+        borderBottomRightRadius: 4,
+        width: '100%',
+        minHeight: 74,
+        flexDirection: 'column',
+        paddingTop: 8,
+        paddingLeft: 16,
+        paddingRight: 16,
+      },
+      subBannerText: {
+        fontSize: 14,
+        color: '#FFFFFF',
+        lineHeight: 21,
+        alignSelf:'center',
+        paddingLeft: 10,
+        paddingTop: 3,
+      },
+      flexColumnContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+      }
 });
--- src/components/WelcomeDialogInner3.tsx
+++ src/components/WelcomeDialogInner3.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { View, StyleSheet, Text, Image, useWindowDimensions } from 'react-native';
+import { useTranslation } from '../services/i18n/i18nUtils' 
 
+const images = {
+    icon: require('../../assets/img/onboardingIcon.png')
+}
 export default WelcomeDialogInner3 = ({width}:{width: number})=> {
     //const { width } = useWindowDimensions();
+    const { t } = useTranslation()
     return (
         <View style={[styles.container, { width }]}>
-            {/* <Image source={item.image} style={[styles.image, { width, resizeMode: 'contain' }]} /> */}
-
-            <View style={{ flex: 0.3 }}>
-                <Text style={styles.title}>HI HI Hi Hi</Text>
-                <Text style={styles.description}>There</Text>
+             <View style={{ flex: .5 }}>
+                <Text style={styles.title}> { t('WelcomeDialog.WelcomeInner3Title', "Don't forget!")} </Text>
+                <Image style={{ alignSelf: 'center' } }source={ images.icon } />
+                <Text style={styles.description}> { t('WelcomeDialog.WelcomeInner3Description', "Always verifiy identity of the SMART Health Card holder wih a government-issued I.D.")} </Text>
             </View>
         </View>
-        
-        
     );
 };
 
@@ -22,7 +24,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: "#FFFFFF"
+        backgroundColor: "#FFFFFF",
+        marginTop: 100
     },
     image: {
         flex: 0.7,
@@ -32,7 +35,7 @@ const styles = StyleSheet.create({
         fontWeight: '800',
         fontSize: 28,
         marginBottom: 10,
-        color: '#493d8a',
+        color: '#052049',
         textAlign: 'center',
         justifyContent: 'center'
     },
@@ -40,7 +43,8 @@ const styles = StyleSheet.create({
         fontWeight: '300',
         color: '#62656b',
         textAlign: 'center',
-        paddingHorizontal: 64
+        paddingHorizontal: 28,
+        paddingTop: 10
     },
     innerComponent: {
         backgroundColor: `#7fff00`,
